c0056e5d69a10c1e52daefe02a452f8496a1be5f
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2022-01-10 12:56:37 +0200

    finish

--- contact.js
+++ contact.js
@@ -1,15 +1,38 @@
-function listContacts() {
-  // ...твой код
+const fs = require('fs/promises')
+const path = require('path')
+const crypto = require('crypto')
+
+
+const readContent = async () => {
+  const contactsPath = await fs.readFile(path.join(__dirname, 'db', 'contacts.json'), 'utf8')
+  const result = JSON.parse(contactsPath)
+  return result
 }
 
-function getContactById(contactId) {
-  // ...твой код
+const listContacts = async() => {
+  return readContent()
 }
 
-function removeContact(contactId) {
-  // ...твой код
+const getContactById = async(contactId) => {
+  const contacts = await readContent()
+  const res = contacts.find(contact => contact.id === contactId)
+  return res
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+const removeContact = async(contactId) => {
+  const contacts = await readContent()
+  const deleteContact = contacts.findIndex(contact => contact.id === contactId)
+  contacts.splice(deleteContact, 1)
+  await fs.writeFile(path.join(__dirname, 'db', 'contacts.json'), JSON.stringify(contacts, null, 2))
+  return deleteContact
 }
+
+const addContact = async(name, email, phone) => {
+  const contacts = await readContent()
+  const newContact = { name, email, phone, id: crypto.randomUUID() }
+  contacts.push(newContact)
+  await fs.writeFile(path.join(__dirname, 'db', 'contacts.json'), JSON.stringify(contacts, null, 2))
+  return newContact
+}
+
+module.exports = {listContacts, getContactById, removeContact, addContact}
--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-console.log('hello world');
+const chalk = require('chalk')
+const { Command } = require('commander')
+const {listContacts, addContact, getContactById, removeContact} = require ('./contact')
+
+const program = new Command();
+program
+  .requiredOption('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+      const contacts = await listContacts()
+      console.table(contacts)
+      break;
+
+    case 'get':
+      const getContact = await getContactById(id)
+      if (getContact) { 
+        console.log(chalk.green('Contact found'))
+        console.log(getContact)
+      }
+      else {
+        console.log(chalk.yellow
+          ('Contact not found'))
+      }
+      break;
+
+    case 'add':
+      if (name && email && phone) {
+        const newContact = await addContact(name, email, phone)
+        console.log(chalk.green('Add new contact'))
+        console.log(newContact)
+      }
+      else {
+        console.log(chalk.red
+          ('Incorrect data'))
+      }
+      break;
+
+    case 'remove':
+      const delContact = await removeContact(id)
+      if (delContact !== -1) {
+        console.log(chalk.green('Contact deleted'))
+      }
+      else {
+        console.log(chalk.yellow('Contact not found'))
+      }
+      break;
+
+    default:
+      console.warn(chalk.red('Unknown action type!'))
+  }
+}
+
+invokeAction(argv)
